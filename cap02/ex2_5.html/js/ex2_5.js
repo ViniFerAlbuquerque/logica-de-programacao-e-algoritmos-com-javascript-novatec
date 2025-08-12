973adf6499417f5bbb920a164b175891325b61b0
ViniFerAlbuquerque <viniferalbuquerque@gmail.com>
2025-08-12 11:16:54 -0300

    Farmácia JS

---
+++ cap02/ex2_5.html/js/ex2_5.js
@@ -0,0 +1,15 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+
+frm.addEventListener("submit", (e) => {
+    const medicamento = frm.inMed.value
+    const preco = Number(frm.inNum.value)
+
+    let promo = Number(Math.floor(preco*2));
+
+    resp1.textContent = `Promoção do medicamento: ${medicamento}`;
+    resp2.textContent = `Leve 2 por R$ ${promo}`;
+
+    e.preventDefault()
+})
