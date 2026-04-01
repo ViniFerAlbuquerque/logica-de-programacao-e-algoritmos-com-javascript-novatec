da9a505ab6cadbd1e11a2788d1a3bdc4d372248c
ViniFerAlbuquerque <viniferalbuquerque@gmail.com>
2026-04-01 17:03:39 -0300

    bairro-de-entrega

---
+++ cap04/ex4_3.js/script.js
@@ -0,0 +1,28 @@
+const bairro = prompt("Bairro de Entrega: ")
+let taxaEntrega
+switch (bairro) {
+    case "Centro":
+        taxaEntrega = 5.00
+        break
+    case "Alto da Glória":
+    case "Alto da XV":
+    case "Batel":
+    case "Bigorrilho":
+    case "Cristo Rei":
+    case "Jardim Botânico":
+    case "Mercês":
+    case "Rebouças":
+    case "São Francisco":
+        taxaEntrega = 7.50
+        break
+    case "Água Verde":
+    case "Campina do Siqueira":
+    case "Portão":
+    case "Vila Izabel":
+        taxaEntrega = 10.00
+        break
+    case "Parolim":
+        taxaEntrega = 999
+        break
+}
+alert(`Taxa R$: ${taxaEntrega.toFixed(2)}`)
