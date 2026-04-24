6fffc8d6315973568b2b255c985fff47fbd6be3f
ViniFerAlbuquerque <viniferalbuquerque@gmail.com>
2026-04-23 23:02:47 -0300

    programa-numeros-decrescentes-js

---
+++ cap05/ex5_2.js/script.js
@@ -0,0 +1,22 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+
+const numero = Number(frm.inNumero.value)
+
+// let resposta =  `Entre ${numero} e 1 : `
+// for (let i = numero; i >  0; i = i - 1) {
+//     resposta = 
+//     resposta + i + ", "
+// }
+// resp.innerText = resposta
+
+let resposta = `Entre ${numero} e 1: ${numero}`
+for (let i = numero - 1; i > 0; i--) {
+    resposta = resposta + ", " + i
+}
+resp.innerText = resposta
+})
