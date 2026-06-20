c16cd2e505cb09f6d890db6d858b2c8f6d8ddc5d
ViniFerAlbuquerque <viniferalbuquerque@gmail.com>
2026-06-20 14:11:47 -0300

    limitador-de-idade

--- cap06/pesquisar-filtrar/script.js
+++ cap06/pesquisar-filtrar/script.js
@@ -4,3 +4,17 @@ for (const idade of idades) {
         console.log(idade)
     }
 }
+
+console.log("========================================================================")
+
+const idades01 = [12, 16, 15, 17, 14]
+let maiores = false
+for (const idade01 of idades01) {
+    if (idade01 >= 18) {
+        console.log(idade01) 
+        maiores = true
+    }
+}
+if (!maiores) {
+    console.log("[ATENÇÂO!] → Não há idades maiores que 18 na lista!")
+}
