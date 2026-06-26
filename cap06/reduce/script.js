205303d33125bca5926e2463f3d8538ad1a9846a
ViniFerAlbuquerque <viniferalbuquerque@gmail.com>
2026-06-25 23:04:43 -0300

    reduce-js

---
+++ cap06/reduce/script.js
@@ -0,0 +1,15 @@
+const numeros = [14, 13, 20, 8, 15]
+const soma = numeros.reduce((acumulador, num) => acumulador + num, 0)
+console.log(`Soma: ${soma}`)
+
+console.log("#####################################################################")
+
+//  const amigos = [{nome: "Ana", idade: 20},
+//                 {nome: "Bruno", idade: 17},
+//                 {nome: "Cátia", idade: 25}]
+
+// const soma = amigos.reduce((acumulador, amigo) => acumulador + amigo.idade, 0)
+// const todos = amigos.reduce((acumulador, amigo) => acumulador + amigo.nome + " ","")   
+
+// console.log(`Soma: ${`soma`}`) 
+// console.log(`Todos: ${todos}`)
