b5dec974bbf671882d05b550c77016d533af5b54
ViniFerAlbuquerque <viniferalbuquerque@gmail.com>
2026-07-21 22:30:32 -0300

    revenda-herbie-listar

--- cap06/ex6_3/script.js
+++ cap06/ex6_3/script.js
@@ -12,3 +12,13 @@ frm.addEventListener("submit", (e) => {
     inModelo.focus()
     frm.btListar.dispatchEvent(new Event("click"))
 })
+
+frm.btListar.addEventListener("click", () => {
+    if(carros.length == 0){
+                   alert("[ATENÇÂO] → Não há carros na lista!")
+            return 
+    }
+    const lista = carros.reduce((acumulador, carro) => 
+    acumulador + carro.modelo + " - R$: " + carro.preco.toFixed(2) + "\n", "")
+    resp.innerText = `Lista dos Carros Cadastrados \n${"-", repeat(40)}\n${lista}`
+})
