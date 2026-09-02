25669a5ec189b84339b7546508a9af2be983a2a5
ViniFerAlbuquerque <viniferalbuquerque@gmail.com>
2026-09-02 18:47:37 -0300

    numeros-em-ordem-js

--- cap06/resp6_2/script.js
+++ cap06/resp6_2/script.js
@@ -3,12 +3,13 @@ const numeros = []
 const inputNumero = document.getElementById('inputNumero')
 const btnAdicionar = document.getElementById('btnAdicionar')
 const btnVerificarOrdem = document.getElementById('btnVerificarOrdem')
+const btnLimparLista = document.getElementById('btnLimparLista');
 const listaNumerosDisplay = document.getElementById('listaNumeros')
 const statusOrdemDisplay = document.getElementById('statusOrdem')
 
 const exibirNumeros = () => {
     if(numeros.length === 0){
-    listaNumerosDisplay.textContent = '[ATENÇÂO] → Nenhum número adicionado ainda!'
+    listaNumerosDisplay.textContent = '[ATENÇÃO] → Nenhum número adicionado ainda!'
 }else{
     listaNumerosDisplay.textContent = `Números: [${numeros.join(', ')}]`
 }
@@ -19,14 +20,14 @@ const verificarOrdemCrescente = () => {
         return true 
     }
     for(let i = 1; i < numeros.length; i++){
-    if(numeros[i] < numeros[i-1]){
+    if(numeros[i] < numeros[i - 1]){
         return false 
     }
  }
     return true 
 }
 
-btnAdicionar.addEventListner('click', () => {
+btnAdicionar.addEventListener('click', () => {
     const numeroDigitado = Number(inputNumero.value)
     if(isNaN(numeroDigitado) || inputNumero.value.trim() === ''){
         alert(`[ATENÇÂO] → Por favor, digite um número válido!`)
@@ -51,7 +52,7 @@ btnAdicionar.addEventListner('click', () => {
 btnVerificarOrdem.addEventListener('click', () => {
     if(numeros.length === 0){
         statusOrdemDisplay.textContent = 'Adicione número para verificar a ordem!'
-        statusOrdemDisplay.style.color ="orage"
+        statusOrdemDisplay.style.color ="orange"
         return 
     }
     const estaEmOrdem = verificarOrdemCrescente()
@@ -59,10 +60,19 @@ btnVerificarOrdem.addEventListener('click', () => {
         statusOrdemDisplay.textContent ='✅Números estão em ordem crescente!'
         statusOrdemDisplay.style.color ='green'
     }else{
+        const numerosOrdenados = [...numeros].sort((a, b) => a - b)
         statusOrdemDisplay.textContent ='❌Números NÃO estão em ordem crescente!'
         statusOrdemDisplay.style.color = 'red'
     }
 })
+
+btnLimparLista.addEventListener('click', () => {
+    numeros.length = 0
+    exibirNumeros()
+    statusOrdemDisplay.textContent = ''
+    statusOrdemDisplay.style.color = ''
+    inputNumero.focus()
+    })
 
    exibirNumeros()
 
