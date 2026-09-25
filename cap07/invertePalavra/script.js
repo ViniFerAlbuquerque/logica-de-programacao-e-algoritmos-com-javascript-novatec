566213ade65244a6f4512938a011037a335aec1e
ViniFerAlbuquerque <viniferalbuquerque@gmail.com>
2026-09-24 21:21:30 -0300

    programa-inverte-palavra

---
+++ cap07/invertePalavra/script.js
@@ -0,0 +1,10 @@
+const palavra = prompt("Palavras: ");
+const tam = palavra.length;
+
+let inverso = palavra.charAt(tam - 1).toUpperCase();
+
+for (let i = tam - 2; i >= 0; i--) {
+    inverso += palavra.charAt(i).toLowerCase();
+};
+
+alert(`Palavra: ${palavra}\nInvertida: ${inverso}`);
